9d9c8ef4b4917480c1d1ef4297581ecc9e307465
Julian Alves <julianalvesinstinto@gmail.com>
2024-11-07 16:55:57 -0300

    adicionando coisas

--- js/jogo.js
+++ js/jogo.js
@@ -1,3 +1,23 @@
+var pontuacao = 0;
+const pontuacaoHtml = document.getElementById('pontuacao');
+pontuacaoHtml.innerHTML = pontuacao;
+
+var maximoPerguntas = 20;
+const perguntasMaximas = document.getElementById('maximoPerguntas');
+perguntasMaximas.innerHTML = maximoPerguntas;
+
+function verificarContador() {
+  // Exibe o valor atual do contado
+  console.log(maximoPerguntas);
+  if (maximoPerguntas <= 0) {
+    // Redireciona para outra página quando o contador atinge zero
+    window.location.href = `./resultado.html?valor=${encodeURIComponent(pontuacao)}`; 
+  }
+}
+
+
+setInterval(verificarContador, 1000);
+
 const perguntas = {
     facil: [
       { pergunta: "Qual o maior rio da Europa?", opcoes: ["Rio Volga (Rússia)", "Rio Danúbio (Europa Central)", "Rio Reno (Europa Ocidental)", "Rio Tâmisa (Reino Unido)"], correta: "Rio Volga (Rússia)" },
@@ -18,6 +38,9 @@ const perguntas = {
 const perguntasMostradas = { facil: [], medio: [], dificil: [] };
 
     function mostrarPergunta() {
+      maximoPerguntas = maximoPerguntas - 1;
+      perguntasMaximas.innerHTML = maximoPerguntas;
+
       const dificuldade = document.querySelector('input[name="dificuldade"]:checked');
       
       if (!dificuldade) {
@@ -60,12 +83,29 @@ const perguntasMostradas = { facil: [], medio: [], dificil: [] };
     }
 
     function verificarResposta(opcaoSelecionada, respostaCorreta) {
+  
+      const dificuldade = document.querySelector('input[name="dificuldade"]:checked');
+      const nivel = dificuldade.value;
       const resultado = document.getElementById('resultado');
       if (opcaoSelecionada === respostaCorreta) {
         resultado.textContent = 'Correto!';
-
+        if(nivel == 'facil'){
+          pontuacao += 1;
+        }
+        else if(nivel == 'medio'){
+          pontuacao += 2;
+        }
+        else if(nivel == 'dificil'){
+          pontuacao = pontuacao + 3;
+          console.log(pontuacao);
+        }
+        else{
+          alert('Algo de errado aconteceu!')
+        }
+        pontuacaoHtml.innerHTML = pontuacao;
       } else {
         resultado.textContent = 'Errado. Tente novamente!';
+        pontuacaoHtml.innerHTML = pontuacao;
       }
 
       document.getElementById('opcoes').style.display = 'none';
